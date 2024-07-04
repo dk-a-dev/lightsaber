591b958f68c31a23e5e037285cf396ce58d67d04
Dev Keshwani <dev.keshwani345@gmail.com>
2024-07-04 14:43:19 +0530

    feat: json data customised

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -9,6 +9,8 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+type envelope map[string]interface{}
+
 func (app *application) readIdParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
@@ -19,22 +21,21 @@ func (app *application) readIdParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-
-func(app *application)writeJson(w http.ResponseWriter,status int,data interface{},headers http.Header)error{
-	js,err:=json.Marshal(data)
-	if(err!=nil){
+func (app *application) writeJson(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
 		return err
 	}
 
-	js=append(js, '\n')
+	js = append(js, '\n')
 
-	for key,value:=range headers{
-		w.Header()[key]=value
+	for key, value := range headers {
+		w.Header()[key] = value
 	}
 
-	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
-	
+
 	return nil
 }
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"lightsabor.dkadev.net/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +18,18 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		http.NotFound(w, r)
 		return
 	}
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Yeh Jawani hai Dewaani",
+		Runtime:   120,
+		Genres:    []string{"drama", "comedy", "romance"},
+		Version:   1,
+	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	err = app.writeJson(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
