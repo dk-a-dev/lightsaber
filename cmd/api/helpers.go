b650c436868bab001bc4795421141344f5a1ec11
Dev Keshwani <dev.keshwani345@gmail.com>
2025-06-15 18:20:48 +0530

    feat: error handling

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -3,11 +3,14 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 
-	"github.com/julienschmidt/httprouter"
 	"maps"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 type envelope map[string]any
@@ -22,16 +25,50 @@ func (app *application) readIdParam(r *http.Request) (int64, error) {
 }
 
 func (app *application) writeJson(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
-	js,err:=json.MarshalIndent(data,"","\t")
-	if err!=nil{
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
 		return err
 	}
-	js=append(js, '\n')
+	js = append(js, '\n')
 	maps.Copy(w.Header(), headers)
 
-	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
 
+	return nil
+}
+
+func (app *application) readJson(r *http.Request, dst any) error {
+	err := json.NewDecoder(r.Body).Decode(dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field !=
+				"" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+
+		default:
+			return err
+		}
+
+	}
 	return nil
 }
