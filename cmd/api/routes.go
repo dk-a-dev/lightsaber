73ee2acddb834e17229c575e870b1423a24a4bce
Dev Keshwani <dev.keshwani345@gmail.com>
2025-06-16 16:41:45 +0530

    feat: implement movie model and CRUD on movies

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,20 +1,21 @@
 package main
 
-import(
-	"net/http"
+import (
 	"github.com/julienschmidt/httprouter"
+	"net/http"
 )
 
-func (app *application)routes()*httprouter.Router {
-	router:=httprouter.New()
-	
-	router.NotFound=http.HandlerFunc(app.notFoundResponse)
-	router.MethodNotAllowed=http.HandlerFunc(app.methodNotAllowedResponse)
+func (app *application) routes() *httprouter.Router {
+	router := httprouter.New()
 
-	router.HandlerFunc(http.MethodGet,"/v1/healthcheck",app.healthCheckHandler)
-	router.HandlerFunc(http.MethodPost,"/v1/movies",app.createMovieHandler)
-	router.HandlerFunc(http.MethodGet,"/v1/movies/:id",app.showMovieHandler)
-	
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.updateMovieHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 	return router
 
 }
