4b98f55a202d5de4ea4e8e1c7774bbd0da837213
Dev Keshwani <dev.keshwani345@gmail.com>
2025-06-17 09:57:56 +0530

    feat: implement movie listing handler and add filtering capabilities

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,12 +6,14 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
 	"maps"
 
 	"github.com/julienschmidt/httprouter"
+	"lightsaber.dkadev.xyz/internal/validator"
 )
 
 type envelope map[string]any
@@ -87,4 +89,33 @@ func (app *application) readJson(w http.ResponseWriter, r *http.Request, dst any
 		return errors.New("body must only contain a single JSON value")
 	}
 	return nil
+}
+
+func (app *application) readString(qs url.Values, key string, defaultValue string) string {
+	s := qs.Get(key)
+	if s == "" {
+		return defaultValue
+	}
+	return s
+}
+
+func (app *application) readCSV(qs url.Values, key string, defaultValue []string) []string {
+	csv := qs.Get(key)
+	if csv == "" {
+		return defaultValue
+	}
+	return strings.Split(csv, ",")
+}
+
+func (app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int {
+	s := qs.Get(key)
+	if s == "" {
+		return defaultValue
+	}
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		v.AddError(key, "must be an integer value")
+		return defaultValue
+	}
+	return i
 }
